3405d1ef9d44a1e4a9016250b5981b77f1b25cc6
mirkokral <75212488+mirkokral@users.noreply.github.com>
2023-12-27 15:54:34 +0100

    test 1 (hope it goes good)

--- esp32_marauder/configs.h
+++ esp32_marauder/configs.h
@@ -14,7 +14,7 @@
   //#define MARAUDER_M5STICKC
   //#define MARAUDER_MINI
   //#define MARAUDER_V4
-  #define MARAUDER_V6
+  //#define MARAUDER_V6
   //#define MARAUDER_V6_1
   //#define MARAUDER_KIT
   //#define GENERIC_ESP32
@@ -22,6 +22,7 @@
   //#define ESP32_LDDB
   //#define MARAUDER_DEV_BOARD_PRO
   //#define XIAO_ESP32_S3
+  #define LILYGO_T_DECK
   //// END BOARD TARGETS
 
   #define MARAUDER_VERSION "v0.13.6"
@@ -47,6 +48,8 @@
     #define HARDWARE_NAME "Flipper Zero Dev Board Pro"
   #elif defined(XIAO_ESP32_S3)
     #define HARDWARE_NAME "XIAO ESP32 S3"
+  #elif defined(LILYGO_T_DECK)
+    #define HARDWARE_NAME "LilyGO T_Deck"
   #else
     #define HARDWARE_NAME "ESP32"
   #endif
@@ -194,6 +197,21 @@
     //#define HAS_TEMP_SENSOR
     //#define HAS_GPS
   #endif
+
+  
+  #ifdef LILYGO_T_DECK
+    //#define FLIPPER_ZERO_HAT
+    #define HAS_BATTERY
+    // #define HAS_BT
+    #define HAS_BUTTONS
+    // #define HAS_NEOPIXEL_LED
+    //#define HAS_PWR_MGMT
+    #define HAS_SCREEN
+    #define HAS_SD
+    #define USE_SD
+    #define HAS_TEMP_SENSOR
+    // #define HAS_GPS
+  #endif
   //// END BOARD FEATURES
 
   //// FLIPPER ZERO HAT SETTINGS
@@ -251,6 +269,14 @@
       #define U_BTN -1
       #define R_BTN -1
       #define D_BTN -1
+    #endif  
+
+    #ifdef LILYGO_T_DECK
+      #define L_BTN 1
+      #define C_BTN 0
+      #define U_BTN 3
+      #define R_BTN 2
+      #define D_BTN 15
     #endif  
 
   #endif
@@ -500,6 +526,63 @@
     
       #define KIT_LED_BUILTIN 13
     #endif
+    #ifdef LILYGO_T_DECK
+      #define SCREEN_CHAR_WIDTH 40
+      #define HAS_ST7789
+    
+      #define BANNER_TEXT_SIZE 2
+
+      #ifndef TFT_WIDTH
+        #define TFT_WIDTH 320
+      #endif
+
+      #ifndef TFT_HEIGHT
+        #define TFT_HEIGHT 240
+      #endif
+
+      #define TFT_DIY
+    
+      #define SCREEN_WIDTH TFT_WIDTH
+      #define SCREEN_HEIGHT TFT_HEIGHT
+      #define HEIGHT_1 TFT_WIDTH
+      #define WIDTH_1 TFT_HEIGHT
+      #define STANDARD_FONT_CHAR_LIMIT (TFT_WIDTH/6) // number of characters on a single line with normal font
+      #define TEXT_HEIGHT 16 // Height of text to be printed and scrolled
+      #define BOT_FIXED_AREA 0 // Number of lines in bottom fixed area (lines counted from bottom of screen)
+      #define TOP_FIXED_AREA 48 // Number of lines in top fixed area (lines counted from top of screen)
+      #define YMAX 320 // Bottom of screen area
+      #define minimum(a,b)     (((a) < (b)) ? (a) : (b))
+      //#define MENU_FONT NULL
+      #define MENU_FONT &FreeMono9pt7b // Winner
+      //#define MENU_FONT &FreeMonoBold9pt7b
+      //#define MENU_FONT &FreeSans9pt7b
+      //#define MENU_FONT &FreeSansBold9pt7b
+      #define BUTTON_SCREEN_LIMIT 12
+      #define BUTTON_ARRAY_LEN 12
+      #define STATUS_BAR_WIDTH 16
+      #define LVGL_TICK_PERIOD 6
+
+      #define FRAME_X 100
+      #define FRAME_Y 64
+      #define FRAME_W 120
+      #define FRAME_H 50
+
+      // Red zone size
+      #define REDBUTTON_X FRAME_X
+      #define REDBUTTON_Y FRAME_Y
+      #define REDBUTTON_W (FRAME_W/2)
+      #define REDBUTTON_H FRAME_H
+
+      // Green zone size
+      #define GREENBUTTON_X (REDBUTTON_X + REDBUTTON_W)
+      #define GREENBUTTON_Y FRAME_Y
+      #define GREENBUTTON_W (FRAME_W/2)
+      #define GREENBUTTON_H FRAME_H
+    
+      #define STATUSBAR_COLOR 0x4A49
+    
+      #define KIT_LED_BUILTIN -1
+    #endif
   
     #ifdef MARAUDER_MINI
       #define SCREEN_CHAR_WIDTH 40
@@ -628,6 +711,24 @@
     #define BUTTON_PADDING 22
     //#define BUTTON_ARRAY_LEN 5
   #endif
+  #ifdef LILYGO_T_DECK
+    #define BANNER_TIME 100
+    
+    #define COMMAND_PREFIX "!"
+    
+    // Keypad start position, key sizes and spacing
+    #define KEY_X 240 // Centre of key
+    #define KEY_Y 50
+    #define KEY_W 320 // Width and height
+    #define KEY_H 22
+    #define KEY_SPACING_X 0 // X and Y gap
+    #define KEY_SPACING_Y 1
+    #define KEY_TEXTSIZE 1   // Font size multiplier
+    #define ICON_W 22
+    #define ICON_H 22
+    #define BUTTON_PADDING 22
+    //#define BUTTON_ARRAY_LEN 5
+  #endif
   
   #ifdef MARAUDER_MINI
     #define BANNER_TIME 50
@@ -714,6 +815,9 @@
     #ifdef XIAO_ESP32_S3
       #define SD_CS 3
     #endif
+    #ifdef LILYGO_T_DECK
+      #define SD_CS 39
+    #endif
 
   #endif
   //// END SD DEFINITIONS
@@ -765,6 +869,8 @@
   #elif defined(MARAUDER_DEV_BOARD_PRO)
     #define MEM_LOWER_LIM 20000
   #elif defined(XIAO_ESP32_S3)
+    #define MEM_LOWER_LIM 20000
+  #elif defined(LILYGO_T_DECK)
     #define MEM_LOWER_LIM 20000
   #endif
   //// END MEMORY LOWER LIMIT STUFF
